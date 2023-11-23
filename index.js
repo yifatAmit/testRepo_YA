de8ce3414ec627a056fa52e604a0ae701e427c48
Yifat Amit <yifat.amit@bayer.com>
2023-11-23 11:01:45 +0200

    change the json body for test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
       statusCode: 200,
-      body: JSON.stringify("Hello from Lambda and Github!"),
+      body: JSON.stringify("Hello from Lambda and Github! test Yifat"),
     }
     return response
   }
